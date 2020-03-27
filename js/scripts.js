f6598d028def99f59c0e6d3200eb486af2e08ec8
William Pratt <willpratt78@gmail.com>
2020-03-27 10:23:55 -0700

    questions, js for variables, night mode

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("button#dark").click(function() {
+    $("body").toggleClass("change-mode");
+
+  var background = $("input#background").val();
+  var critical = $("input#critical").val();
+  var life = $("input#life").val();
+  var job = $("input#job").val();
+  var time = $("input#time").val();
+
+  var result;
+  
+    event.preventDefault();
+  });
+});
