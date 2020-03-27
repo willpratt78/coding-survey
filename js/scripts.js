1722e4cb0aaf2e4b8d4184942892f69c85afb1d5
William Pratt <willpratt78@gmail.com>
2020-03-27 11:13:59 -0700

    add variables, if statement, code broken

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
-  $("button#dark").click(function() {
-    $("body").toggleClass("change-mode");
-
-  var background = $("input#background").val();
-  var critical = $("input#critical").val();
-  var life = $("input#life").val();
-  var job = $("input#job").val();
-  var time = $("input#time").val();
-
-  var result;
   
+  
+
+  $("form#language").submit(function(event) {
+    var background = $('input#background').val();
+    var critical = $("input#critical").val();
+    var life = $("input#life").val();
+    var job = $("input#job").val();
+    var time = $('input#time').val();
+   
+
+    var result;
+
+    if (background === a) {
+      result = 'python';
+  } else{
+    result = 'Java'
+  }
+
+    $('#output').text(result);
+
     event.preventDefault();
+  });
   });
 });
